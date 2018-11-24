1dc3b23839e2cea275e3fa510be92cd40bedb0ab
Sergey Golubov <sergey.golubov@ocado.com>
2018-11-24 22:43:43 +0100

    Sex constant + age nullability validation

--- src/main/java/com/hacklek/dtos/UserDataDto.java
+++ src/main/java/com/hacklek/dtos/UserDataDto.java
@@ -1,16 +1,20 @@
 package com.hacklek.dtos;
 
+import com.hacklek.constants.Sex;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @ToString
 public class UserDataDto {
 
+    @NotNull
     private String age;
-    private boolean sex;
+    private Sex sex;
     private boolean alcohol;
     private boolean pregnancy;
 
