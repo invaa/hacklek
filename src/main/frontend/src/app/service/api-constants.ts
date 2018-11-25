030f72ddc62aee74afff3f4ea0ce62161854dc11
Sergey Golubov <sergey.golubov@ocado.com>
2018-11-25 04:54:46 +0100

    Screen 2.

--- src/main/frontend/src/app/service/api-constants.ts
+++ src/main/frontend/src/app/service/api-constants.ts
@@ -3,6 +3,7 @@ export class ApiConstants {
   private static API_ROOT_URL = '/api/v1';
 
   public static API_LOOKUP_MED = ApiConstants.API_ROOT_URL + '/lookup';
+  public static API_OCR_MED = ApiConstants.API_ROOT_URL + '/parseImage';
   public static API_ALTERNATIVES = ApiConstants.API_ROOT_URL + '/alternatives';
 
 }
